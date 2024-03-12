dbce67e440f9e2d9ea6349f7b5a14811bccd68ba
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-03-12 04:45:18 -0400

    We refactored all the ES6 code into ES5

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,10 @@
 // Your code here
+Array.prototype.isEqual = function (input) {
+    if (!(input instanceof Array)) return false;
+
+    for (let i = 0; i < this.length; i++) {
+        if(this[i] !== input[i]) return false;
+    }
+
+    return true;
+}
